9e7ef9a301173f62def01606d8c50e7c92ce4394
lawrst <lawrencelopesgomes@gmail.com>
2024-08-18 21:25:09 -0300

    Finalizado

--- src/main/sorter/MergeSort.java
+++ src/main/sorter/MergeSort.java
@@ -4,11 +4,47 @@ public class MergeSort implements Sorter {
 
     @Override
     public int[] sort(int[] values) {
-        // mergesort implementation
-        return values;
+        if (values.length <= 1) {
+            return values;
+        }
+
+        int mid = values.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[values.length - mid];
+
+        for (int i = 0; i < mid; i++){
+            left[i] = values[i];
+        }
+        for (int i = mid; i < values.length; i++){
+            right[i - mid] = values[i];
+        }
+
+        return merge(sort(left), sort(right));
+
     }
 
     public int[] merge(int[] left, int[] right) {
-        return null;
+        int[] temp = new int [left.length + right.length];
+        int inicio = 0;
+        int fim = 0;
+        int k = 0;
+
+        while(inicio < left.length && fim < right.length){
+            if (inicio < left.length && fim < right.length){
+                if(left[inicio] <= right[fim]){
+                    temp[k++] = left[inicio++];
+                } else {
+                    temp[k++] = right[fim++];
+                }
+            }
+
+        }
+        while(inicio < left.length){
+            temp[k++] = left[inicio++];
+        }
+        while (fim < right.length) {
+            temp[k++] = right[fim++];
+        }
+        return temp;
+        }
     }
-}
